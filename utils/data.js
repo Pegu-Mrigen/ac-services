c45372db07453468ab7dd00eadb7f3fcd6bc41f9
Mrigen <mrig8520@gmail.com>
2023-03-25 16:32:46 +0530

    done

--- utils/data.js
+++ utils/data.js
@@ -1,40 +1,40 @@
 const items = [
   {
     name: "Tea",
-    price: 1,
+    price: 5,
     category: "drinks",
     image:
       "https://img.etimg.com/photo/msid-69212931,quality-100/chai-itself-was-once-a-trend-that-developed-this-way-.jpg",
   },
   {
     name: "coffee",
-    price: 2,
+    price: 10,
     category: "drinks",
     image: "https://i.cdn.newsbytesapp.com/images/l110_14211532861690.jpg",
   },
   {
     name: "Chicken Biryani",
-    price: 10,
+    price: 50,
     category: "rice",
     image:
       "https://www.licious.in/blog/wp-content/uploads/2020/12/Hyderabadi-chicken-Biryani.jpg",
   },
   {
     name: "Veg Biryani",
-    price: 5,
+    price: 40,
     category: "rice",
     image:
       "https://www.indianveggiedelight.com/wp-content/uploads/2020/04/veg-biryani-instant-pot-featured.jpg",
   },
   {
     name: "Chicken Hakka",
-    price: 7,
+    price: 70,
     category: "noodles",
     image: "https://cravecookclick.com/wp-content/uploads/2012/07/IMG_4400.jpg",
   },
   {
     name: "Eggy",
-    price: 5,
+    price: 20,
     category: "noodles",
     image:
       "https://www.licious.in/blog/wp-content/uploads/2020/12/Egg-Noodles-min.jpg",
